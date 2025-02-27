78a519c5b5d59b0b0980b3af3e417634e92363c0
Salvador de Jesus Ron Osio <56851020+salvadorron@users.noreply.github.com>
2025-02-27 09:58:42 -0400

    Update summaryPublicEntity.js

--- src/models/summaryPublicEntity.js
+++ src/models/summaryPublicEntity.js
@@ -1,4 +1,4 @@
-import { Model, UUID, UUIDV4, BOOLEAN } from "sequelize";
+import { Model, UUID, UUIDV4, BOOLEAN, DATEONLY } from "sequelize";
 
 export default (sequelize) => {
   class SummaryPublicEntity extends Model {
